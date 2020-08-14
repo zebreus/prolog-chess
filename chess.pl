a834620c8e708d0b1f5e302b69b2c98520585a6f
Kyrill Abrams <kyrill.abrams@gmx.de>
2020-08-14 04:02:01 +0200

    Added simple sanity check to input

--- chess.pl
+++ chess.pl
@@ -30,12 +30,30 @@ collision([ThisPiece | Rest], [X,Y], Piece) :-
   Piece = ThisPiece;
   collision(Rest,[X,Y],Piece).
 
-
 % input(-XStart, -YStart, -XEnd, -YEnd)
+% Gets valid user input
 input(XStart, YStart, XEnd, YEnd) :-
+  readInput(XStart, YStart, XEnd, YEnd),
+  checkInput(XStart, YStart, XEnd, YEnd), !.
+input(XStart, YStart, XEnd, YEnd) :-
+  write('Invalid input, please check that you have the right format!'),nl,
+  input(XStart, YStart, XEnd, YEnd).
+
+% readInput(-XStart, -YStart, -XEnd, -YEnd)
+% Reads and parses the input from the user
+readInput(XStart, YStart, XEnd, YEnd) :-
   write('Please enter your next move [Format: "A2,A4".]:'), nl,
   read(Input),
   string_codes(Input, [InputXS|[InputYS|[_|[InputXE|[InputYE|_]]]]]),
   XStart is InputXS - 64, XEnd is InputXE - 64,
-  YStart is InputYS - 48, YEnd is InputYE - 48.
-  % write(XStart),write("|"),write(YStart),nl,write(XEnd),write("|"),write(YEnd).
+  YStart is InputYS - 48, YEnd is InputYE - 48,
+  write(XStart),write("|"),write(YStart),nl,write(XEnd),write("|"),write(YEnd).
+
+% checkInput(+XS, +YS, +XE, +YE)
+% Ensures, that only valid numbers are used in the input
+checkInput(XS, YS, XE, YE) :-
+  XS >= 1, XS =< 8,
+  YS >= 1, YS =< 8,
+  XE >= 1, XE =< 8,
+  YE >= 1, YE =< 8.
+  % Add further checks here
